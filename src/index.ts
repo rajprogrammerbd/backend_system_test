55312a50b9590c51b06990949ce0578c4618960b
Raj Dutta <rd2249619@gmail.com>
2022-10-28 23:41:24 +0600

    add the structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-function addUp(a: number, b: number): number {
-    return a + b;
-}
+require('express-async-errors');
+require('dotenv').config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import Database from "./config/db.config"
+import homepageRoutes from './routes/homepage.route';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-export default addUp;
+process.on('uncaughtException', err => {
+  console.log(`Uncaught Exception logged:`, err, err.stack);
+});
+
+process.on('unhandledRejection', error => {
+  console.error('unhandledRejection', error);
+});
+
+// Check for database connection.
+Database.connect().then(() => {
+  console.log('Database (MongoDB) is connected');
+});
+
+const app: express.Application = express();
+
+app.use(cookieParser());
+app.use(express.json());
+
+const corsConfig = {
+  credentials: true,
+  origin: true,
+};
+
+app.use(cors(corsConfig));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use('/api', homepageRoutes);
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({'message': err.message});
+    
+    return;
+});
+
+const PORT = process.env.PORT || 5000;
+export const appPort = app.listen(PORT, () => console.log(`Application is running on ${PORT}`));
--- src/integration/index.test.ts
+++ src/integration/index.test.ts
@@ -1,7 +1,4 @@
-import addUp from ".."
-
 test('test addUp function', () => {
-    const value = addUp(5, 6);
-    
-    expect(value).toBe(11);
+
+    expect(true).toBeTruthy();
 })
